e62485c0dbff6ded8a65057850a374f23ece6d2e
libaicheng <1477996221@qq.com>
2024-08-15 19:20:42 +0800

    feat: 共享libc-shared子包

--- packages/vue-dome1/src/views/HomeView.vue
+++ packages/vue-dome1/src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <script setup>
+import { isObject, isFunction } from '@libc/shared'
 </script>
 
 <template>
   <main class="main-wrapper">
     <section>
-      <p>libc-shared公共方法的使用</p>
+      <h1>libc-shared公共方法的使用</h1>
+      <p>libc-shared中判断是否是对象的方法</p>
+      <p>{{ isObject }}</p>
+      <p>libc-shared中判断是否是函数的方法</p>
+      <p>{{ isFunction }}</p>
     </section>
     <section>
-      <p>libc-ui公共组件库的使用</p>
+      <h1>libc-ui公共组件库的使用</h1>
     </section>
   </main>
 </template>
@@ -16,16 +21,20 @@
 .main-wrapper{
   section{
     padding: 4px 10px;
-    min-height: 120px;
+    min-height: 150px;
     border: 2px solid #de5e60;
     border-radius: 4px;
-    >p{
-      font-size: 16px;
+    >h1{
+      font-size: 18px;
       font-weight: bolder;
       color: #00BD7E;
+      margin-bottom: 4px;
     }
     &:first-child{
       margin-bottom: 12px;
+    }
+    p:nth-child(3){
+      margin-bottom: 6px;
     }
   }
 }
